5075c2ccd95a70cdde9bf3359c6553e8f0fb8a17
priiyascoopy <46895876+dineshsoundar11@users.noreply.github.com>
2020-08-20 21:16:31 +0530

    Maj Rel

--- js/main.js
+++ js/main.js
@@ -1,131 +1,324 @@
-;(function () {
-
-    'use strict';
-
-    var carousels = function() {
-        $(".owl-carousel1").owlCarousel(
-            {
-                loop:true,
-                center: true,
-                margin:0,
-                responsiveClass:true,
-                nav:false,
-                responsive:{
-                    0:{
-                        items:1,
-                        nav:false
-                    },
-                    600:{
-                        items:1,
-                        nav:false
-                    },
-                    1000:{
-                        items:3,
-                        nav:true,
-                        loop:false
-                    }
-                }
-            }
-        );
-
-        $(".owl-carousel2").owlCarousel(
-            {
-                loop:true,
-                center: false,
-                margin:0,
-                responsiveClass:true,
-                nav:true,
-                responsive:{
-                    0:{
-                        items:1,
-                        nav:false
-                    },
-                    600:{
-                        items:2,
-                        nav:false
-                    },
-                    1000:{
-                        items:3,
-                        nav:true,
-                        loop:true
-                    }
-                }
-            }
-        );
-    }
+/* =================================
+------------------------------------
+	Arcade - Architecture
+	Version: 1.0
+ ------------------------------------ 
+ ====================================*/
 
 
-    // svg responsive in mobile mode
-    var checkPosition = function() {
-        if ($(window).width() < 767) {
-            $("#bg-services").attr("viewBox", "0 0 1050 800");
 
+'use strict';
+
+var window_w = $(window).innerWidth();
+
+
+$(window).on('load', function() { 
+	/*------------------
+		Preloder
+	--------------------*/
+	$(".loader").fadeOut(); 
+	$("#preloder").delay(400).fadeOut("slow");
+
+	__portfolio(); // call portfolio function
+
+});
+
+
+(function($) {
+
+	/*------------------
+		Navigation
+	--------------------*/
+	$('.nav-switch').on('click', function(event) {
+		$('.nav-menu').slideToggle(400);
+		event.preventDefault();
+	});
+
+
+
+	/*------------------
+		Background set
+	--------------------*/
+	$('.set-bg').each(function() {
+		var bg = $(this).data('setbg');
+		$(this).css('background-image', 'url(' + bg + ')');
+	});
+
+
+	/*------------------
+		Hero Slider
+	--------------------*/
+	var hero_s = $(".hero-slider");
+    hero_s.owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: true,
+        items: 1,
+        dots: false,
+        animateOut: 'fadeOutRight',
+    	animateIn: 'fadeInLeft',
+        navText: ['<i class="fa fa-long-arrow-left"></i> PREV', 'NEXT<i class="fa fa-long-arrow-right"></i>'],
+        smartSpeed: 1200,
+        autoHeight: false,
+        //autoplay: true,
+        mouseDrag: false,
+        onInitialized: function() {
+        	var a = this.items().length;
+        	if(a < 10){
+            	$("#snh-1").html("<span>01" + " / </span>0" + a);
+       		} else{
+       			$("#snh-1").html("<span>01" + " / </span>" + a);
+       		}
         }
-    };
+    }).on("changed.owl.carousel", function(a) {
+        var b = --a.item.index, a = a.item.count;
+        if(a < 10){
+        	$("#snh-1").html("<span>0" + ( 1 > b ? b + a : b > a ? b - a : b) + " / </span>0" + a);
+    	} else{
+    		$("#snh-1").html("<span> "+ (1 > b ? b + a : b > a ? b - a : b) + " / </span>" + a);
+    	}
+    });
 
-    (function($) {
-        carousels();
-        checkPosition();
-    })(jQuery);
 
 
-}());
+	/*------------------
+		Projects Slider
+	--------------------*/
+	var project = $('#projects-carousel').owlCarousel({
+		nav: true,
+		loop: true,
+		margin:20,
+		navText: ['<i class="fa fa-long-arrow-left"></i> PREV', 'NEXT<i class="fa fa-long-arrow-right"></i>'],
+		responsive:{
+			0:{
+				items:1,
+				margin: 0
+			},
+			600:{
+				items:2
+			},
+			800:{
+				items:2
+			},
+			992:{
+				items:3
+			},
+			1200:{
+				items:4
+			},
+		}
+	});
+	/* animate filter */
+	var owlAnimateFilter = function(even) {
+		$(this)
+		.addClass('__loading')
+		.delay(70 * $(this).parent().index())
+		.queue(function() {
+			$(this).dequeue().removeClass('__loading')
+		});
+	}
+	/* Projects filter */
+	$('.projects-filter-nav li').on('click', function(e) {
+		var filter_data = $(this).data('filter');
 
-// menu toggle button
-function myFunction(x) {
-    x.classList.toggle("change");
+		/* return if current */
+		if($(this).hasClass('btn-active')) return;
+
+		/* active current */
+		$(this).addClass('btn-active').siblings().removeClass('btn-active');
+
+		/* Filter */
+		project.owlFilter(filter_data, function(_owl) { 
+			$(_owl).find('.single-project').each(owlAnimateFilter); 
+		});
+	});
+
+
+
+	/*------------------
+		Brands Slider
+	--------------------*/
+	$('#client-carousel').owlCarousel({
+		nav: false,
+		loop: true,
+		margin:20,
+		autoplay: true,
+		responsive:{
+			0:{
+				items:1,
+				margin: 0
+			},
+			600:{
+				items:3
+			},
+			800:{
+				items:4
+			},
+			992:{
+				items:4
+			},
+			1200:{
+				items:5
+			},
+		}
+	});
+
+
+
+	/*------------------
+		Review Slider
+	--------------------*/
+	var test_s = $("#test-slider");
+    test_s.owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: false,
+        items: 1,
+        dots: false,
+        autoplay: true,
+        onInitialized: function() {
+        	var a = this.items().length;
+        	if(a < 10){
+            	$("#snh-2").html("<span>01" + "/ </span>0" + a);
+       		} else{
+       			$("#snh-2").html("<span>01" + "/ </span>" + a);
+       		}
+        }
+    }).on("changed.owl.carousel", function(a) {
+        var b = --a.item.index, a = a.item.count;
+        if(a < 10){
+        	$("#snh-2").html("<span>0" + ( 1 > b ? b + a : b > a ? b - a : b) + "/ </span>0" + a);
+    	} else{
+    		$("#snh-2").html("<span> "+ (1 > b ? b + a : b > a ? b - a : b) + "/ </span>" + a);
+    	}
+    });
+
+
+
+    /*------------------
+		Service Slider
+	--------------------*/
+	$('.service-slider').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: false,
+        items: 1,
+        dots: true,
+        autoplay: true,
+    });
+
+
+
+	/*------------------
+		Popup
+	--------------------*/
+	$('.img-popup').magnificPopup({
+		type: 'image',
+		mainClass: 'img-popup-warp',
+		removalDelay: 400,
+	});
+
+
+	/*------------------
+		Accordions
+	--------------------*/
+	$('.panel-link').on('click', function (e) {
+		$('.panel-link').parent('.panel-header').removeClass('active');
+		var $this = $(this).parent('.panel-header');
+		if (!$this.hasClass('active')) {
+			$this.addClass('active');
+		}
+		e.preventDefault();
+	});
+
+
+
+	//Set progress circle 1
+	$("#progress1").circleProgress({
+		value: 0.75,
+		size: 195,
+		thickness: 20,
+		fill: "#baff00",
+		emptyFill: "rgba(0, 0, 0, 0)"
+	});
+	//Set progress circle 2
+	$("#progress2").circleProgress({
+		value: 0.83,
+		size: 195,
+		thickness: 20,
+		fill: "#baff00",
+		emptyFill: "rgba(0, 0, 0, 0)"
+	});
+	//Set progress circle 3
+	$("#progress3").circleProgress({
+		value: 0.25,
+		size: 195,
+		thickness: 20,
+		fill: "#baff00",
+		emptyFill: "rgba(0, 0, 0, 0)"
+	});
+	//Set progress circle 4
+	$("#progress4").circleProgress({
+		value: 0.95,
+		size: 195,
+		thickness: 20,
+		fill: "#baff00",
+		emptyFill: "rgba(0, 0, 0, 0)"
+	});
+
+})(jQuery);
+
+
+/*------------------
+	Portfolio 
+--------------------*/
+function __portfolio(){
+
+	portfolio_item_size();
+
+	$(window).on('resize', function(){
+		portfolio_item_size();
+	});
+
+
+	var $container = $('#portfolio');
+	$container.isotope({
+		itemSelector: '.grid-item',
+		percentPosition: true,
+		masonry: {
+			columnWidth: '.grid-sizer'
+		}
+	});
+
+	// portfolio filter nav
+	$('.portfolio-filter li').on("click", function(){
+		$(".portfolio-filter li").removeClass("active");
+		$(this).addClass("active");				 
+		var selector = $(this).attr('data-filter');
+		$container.isotope({
+				filter: selector,
+				animationOptions: {
+				duration: 750,
+				easing: 'linear',
+				queue: false,
+			}
+		});
+		return false;
+	});
 }
-var words = document.getElementsByClassName('word');
-var wordArray = [];
-var currentWord = 0;
 
-words[currentWord].style.opacity = 1;
-for (var i = 0; i < words.length; i++) {
-  splitLetters(words[i]);
+/*------------------
+	Portfolio grid
+--------------------*/
+function portfolio_item_size(){
+	$('#portfolio').find('.grid-item').each(function() {
+		var pi_height1 = $(this).outerWidth(true),
+		pi_height2 = pi_height1/2;
+		
+		if($(this).hasClass('grid-long') && window_w > 991){
+			$(this).css('height', pi_height2);
+		}else{
+			$(this).css('height', Math.abs(pi_height1));
+		}
+	});
 }
-
-function changeWord() {
-  var cw = wordArray[currentWord];
-  var nw = currentWord == words.length-1 ? wordArray[0] : wordArray[currentWord+1];
-  for (var i = 0; i < cw.length; i++) {
-    animateLetterOut(cw, i);
-  }
-
-  for (var i = 0; i < nw.length; i++) {
-    nw[i].className = 'letter behind';
-    nw[0].parentElement.style.opacity = 1;
-    animateLetterIn(nw, i);
-  }
-
-  currentWord = (currentWord == wordArray.length-1) ? 0 : currentWord+1;
-}
-
-function animateLetterOut(cw, i) {
-  setTimeout(function() {
-		cw[i].className = 'letter out';
-  }, i*80);
-}
-
-function animateLetterIn(nw, i) {
-  setTimeout(function() {
-		nw[i].className = 'letter in';
-  }, 340+(i*80));
-}
-
-function splitLetters(word) {
-  var content = word.innerHTML;
-  word.innerHTML = '';
-  var letters = [];
-  for (var i = 0; i < content.length; i++) {
-    var letter = document.createElement('span');
-    letter.className = 'letter';
-    letter.innerHTML = content.charAt(i);
-    word.appendChild(letter);
-    letters.push(letter);
-  }
-
-  wordArray.push(letters);
-}
-
-changeWord();
-setInterval(changeWord, 4000);
